aa85428cf1fca1c109f1fc85a1fd0b4c4717f1f3
Harsha <137918319+CoderHarshaVardhan@users.noreply.github.com>
2024-12-09 19:01:50 +0530

    Add files via upload

---
+++ statefullwidget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Stateful Widget Example")),
+        body: Center(
+          child: CounterWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter = 0;
+  // Method to increment the counter
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Counter: $_counter",
+          style: TextStyle(fontSize: 24),
+        ),
+        SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: _incrementCounter,
+          child: Text("Increment"),
+        ),
+      ],
+    );
+  }
+}
